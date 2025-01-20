c8b05f60bc391f612bc5ff3b0681f8934ae5ecab
Clo91eaf <Clo91eaf@qq.com>
2025-01-20 23:16:31 +0800

    [cover] add trait definitions for instruction and constraint generation
    Signed-off-by: Clo91eaf <Clo91eaf@qq.com>

--- cover/src/Api.scala
+++ cover/src/Api.scala
@@ -2,3 +2,22 @@
 // SPDX-FileCopyrightText: 2025 Jiuyang Liu <liu@jiuyang.me>
 
 package cover
+
+trait Instruction {
+  def name: String
+  def attributes: Map[String, Any]
+}
+
+trait Constraint {
+  def apply(attributes: Map[String, Any]): Boolean
+}
+
+trait InstructionGenerator {
+  def instruction(name: String, count: Int): Unit
+
+  def constraint(constraint: Constraint): Unit
+
+  def build(): Seq[Instruction]
+
+  def clear(): Unit
+}
