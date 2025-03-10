be58dc9231460e2ea47f7debfb6c3678232f2192
Clo91eaf <Clo91eaf@qq.com>
2025-03-10 17:43:43 +0800

    [circtlib] add smt dialect support
    Signed-off-by: Clo91eaf <Clo91eaf@qq.com>

---
+++ circtlib/src/smt/operator/TypeAPI.scala
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: Apache-2.0
+// SPDX-FileCopyrightText: 2025 Jianhao Ye <Clo91eaf@qq.com>
+package org.llvm.circt.scalalib.smt.tpe
+
+class ArrayType
+class BitVectorType
+class BoolType
+class IntType
+class SMTFuncType
+class SortType
