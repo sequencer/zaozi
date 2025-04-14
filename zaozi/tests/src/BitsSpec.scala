5770967e3301547c547ed2b7bbc7c181f95524bf
unlsycn <unlsycn@unlsycn.com>
2025-04-14 11:33:27 +0800

    test(zaozi): add tests for Vec and Bits extraction
    Signed-off-by: unlsycn <unlsycn@unlsycn.com>

--- zaozi/tests/src/BitsSpec.scala
+++ zaozi/tests/src/BitsSpec.scala
@@ -5,6 +5,11 @@ package me.jiuyang.zaozi.tests
 import me.jiuyang.zaozi.*
 import me.jiuyang.zaozi.default.{*, given}
 import me.jiuyang.zaozi.reftpe.*
+
+import org.llvm.circt.scalalib.firrtl.capi.given_DialectHandleApi
+import org.llvm.mlir.scalalib.{given_ContextApi, Context, ContextApi}
+
+import java.lang.foreign.Arena
 import utest.*
 
 class BitsSpecIO(
@@ -265,3 +270,87 @@ object BitsSpec extends TestSuite:
         io.bits.dontCare()
         io.widenBits.dontCare()
         io.bits := io.a.bits(4, 3)
+    test("ExtractRange apply"):
+      verilogTest(new BitsSpecIO, new BitsSpecProbe)(
+        "assign bits = {6'h0, a[4:3]};"
+      ):
+        val io = summon[Interface[BitsSpecIO]]
+        io.sint.dontCare()
+        io.uint.dontCare()
+        io.bool.dontCare()
+        io.bits.dontCare()
+        io.widenBits.dontCare()
+        io.bits := io.a(4, 3)
+    test("Named ExtractRange apply"):
+      verilogTest(new BitsSpecIO, new BitsSpecProbe)(
+        "assign bits = {6'h0, a[4:3]};"
+      ):
+        val io = summon[Interface[BitsSpecIO]]
+        io.sint.dontCare()
+        io.uint.dontCare()
+        io.bool.dontCare()
+        io.bits.dontCare()
+        io.widenBits.dontCare()
+        io.bits := io.a(hi = 4, lo = 3)
+    test("ExtractElement"):
+      verilogTest(new BitsSpecIO, new BitsSpecProbe)(
+        "assign bits = {7'h0, a[4]};"
+      ):
+        val io = summon[Interface[BitsSpecIO]]
+        io.sint.dontCare()
+        io.uint.dontCare()
+        io.bool.dontCare()
+        io.bits.dontCare()
+        io.widenBits.dontCare()
+        io.bits := io.a.bit(4)
+    test("ExtractElement apply"):
+      verilogTest(new BitsSpecIO, new BitsSpecProbe)(
+        "assign bits = {7'h0, a[4]};"
+      ):
+        val io = summon[Interface[BitsSpecIO]]
+        io.sint.dontCare()
+        io.uint.dontCare()
+        io.bool.dontCare()
+        io.bits.dontCare()
+        io.widenBits.dontCare()
+        io.bits := io.a(4)
+    test("Named ExtractElement apply"):
+      verilogTest(new BitsSpecIO, new BitsSpecProbe)(
+        "assign bits = {7'h0, a[4]};"
+      ):
+        val io = summon[Interface[BitsSpecIO]]
+        io.sint.dontCare()
+        io.uint.dontCare()
+        io.bool.dontCare()
+        io.bits.dontCare()
+        io.widenBits.dontCare()
+        io.bits := io.a(idx = 4)
+
+    given Arena   = Arena.ofConfined()
+    given Context = summon[ContextApi].contextCreate
+    summon[Context].loadFirrtlDialect()
+    test("Apply with wrong data type"):
+      summon[ConstructorApi].Module(new BitsSpecIO, new BitsSpecProbe):
+        val io = summon[Interface[BitsSpecIO]]
+        compileError("""io.bits := io.c(1)""").check(
+          "",
+          "Element type must be Bits, but got me.jiuyang.zaozi.valuetpe.UInt."
+        )
+    test("Apply with incorrect named argument"):
+      summon[ConstructorApi].Module(new BitsSpecIO, new BitsSpecProbe):
+        val io = summon[Interface[BitsSpecIO]]
+        compileError("""io.bits := io.a(invalid_arg = 4)""").check(
+          "",
+          "missing argument for parameter idx of method bitsApplyBitWrapper in object ApplyHelper"
+        )
+        compileError("""io.bits := io.a(idx = 1, invalid_arg = 4)""").check(
+          "",
+          "missing argument for parameter hi of method bitsApplyBitsWrapper in object ApplyHelper"
+        )
+    test("Apply with incorrect number of arguments"):
+      summon[ConstructorApi].Module(new BitsSpecIO, new BitsSpecProbe):
+        val io = summon[Interface[BitsSpecIO]]
+        compileError("""io.bits := io.b(1, 2, 3)""").check(
+          "",
+          "Expected 1 or 2 args, but got 3"
+        )
