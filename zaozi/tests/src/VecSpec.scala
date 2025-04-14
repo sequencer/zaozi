bc29a70536fb860dc5aa706e82582f940978dc09
unlsycn <unlsycn@unlsycn.com>
2025-04-14 11:33:27 +0800

    test(zaozi): add tests for Vec and Bits extraction
    Signed-off-by: unlsycn <unlsycn@unlsycn.com>

--- zaozi/tests/src/VecSpec.scala
+++ zaozi/tests/src/VecSpec.scala
@@ -5,6 +5,11 @@ package me.jiuyang.zaozi.tests
 import me.jiuyang.zaozi.*
 import me.jiuyang.zaozi.default.{*, given}
 import me.jiuyang.zaozi.reftpe.Interface
+
+import org.llvm.circt.scalalib.firrtl.capi.given_DialectHandleApi
+import org.llvm.mlir.scalalib.{given_ContextApi, Context, ContextApi}
+
+import java.lang.foreign.Arena
 import utest.*
 
 case class VecSpecParameter(width: Int, vecCount: Int) extends Parameter:
@@ -27,24 +32,63 @@ class VecSpecProbe(
 object VecSpec extends TestSuite:
   val tests = Tests:
     given VecSpecParameter(8, 4)
-    test("Vec should work"):
+    test("Assign"):
       firrtlTest(new VecSpecIO, new VecSpecProbe)(
         "connect io.b, io.a"
       ):
         val io = summon[Interface[VecSpecIO]]
         io.b := io.a
         io.out.dontCare()
-    test("Vec dynamic idx work"):
+    test("Dynamic index"):
+      firrtlTest(new VecSpecIO, new VecSpecProbe)(
+        "node tests = io.a[io.idx]"
+      ):
+        val io = summon[Interface[VecSpecIO]]
+        io.b.dontCare()
+        io.out := io.a.bit(io.idx)
+    test("Dynamic index apply"):
       firrtlTest(new VecSpecIO, new VecSpecProbe)(
         "node tests = io.a[io.idx]"
       ):
         val io = summon[Interface[VecSpecIO]]
         io.b.dontCare()
         io.out := io.a(io.idx)
-    test("Vec static idx work"):
+    test("Static index"):
+      firrtlTest(new VecSpecIO, new VecSpecProbe)(
+        "node tests = io.a[3]"
+      ):
+        val io = summon[Interface[VecSpecIO]]
+        io.b.dontCare()
+        io.out := io.a.bit(3)
+    test("Static index apply"):
       firrtlTest(new VecSpecIO, new VecSpecProbe)(
         "node tests = io.a[3]"
       ):
         val io = summon[Interface[VecSpecIO]]
         io.b.dontCare()
         io.out := io.a(3)
+    test("Named static index apply"):
+      firrtlTest(new VecSpecIO, new VecSpecProbe)(
+        "node tests = io.a[3]"
+      ):
+        val io = summon[Interface[VecSpecIO]]
+        io.b.dontCare()
+        io.out := io.a(idx = 3)
+
+    given Arena   = Arena.ofConfined()
+    given Context = summon[ContextApi].contextCreate
+    summon[Context].loadFirrtlDialect()
+    test("Apply with incorrect named argument"):
+      summon[ConstructorApi].Module(new VecSpecIO, new VecSpecProbe):
+        val io = summon[Interface[VecSpecIO]]
+        compileError("""io.out := io.a(invalid_name = 3)""").check(
+          "",
+          "missing argument for parameter idx of method vecApplyWrapper in object ApplyHelper"
+        )
+    test("Apply with incorrect number of arguments"):
+      summon[ConstructorApi].Module(new VecSpecIO, new VecSpecProbe):
+        val io = summon[Interface[VecSpecIO]]
+        compileError("""io.out := io.a(1, 2)""").check(
+          "",
+          "Expected 1 args, but got 2"
+        )
