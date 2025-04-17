490d5a4b73c3f625d1a51182fe524dabcc402b67
unlsycn <unlsycn@unlsycn.com>
2025-04-17 23:57:00 +0800

    refactor(zaozi): split default operators into multiple files
    Signed-off-by: unlsycn <unlsycn@unlsycn.com>

---
+++ zaozi/src/default/BoolApi.scala
@@ -0,0 +1,206 @@
+// SPDX-License-Identifier: Apache-2.0
+// SPDX-FileCopyrightText: 2025 Jiuyang Liu <liu@jiuyang.me>
+package me.jiuyang.zaozi.default
+
+import me.jiuyang.zaozi.BoolApi
+import me.jiuyang.zaozi.reftpe.*
+import me.jiuyang.zaozi.valuetpe.*
+
+import org.llvm.circt.scalalib.firrtl.capi.{given_FirrtlBundleFieldApi, given_TypeApi, FirrtlNameKind}
+import org.llvm.circt.scalalib.firrtl.operation.{
+  AndPrimApi,
+  EQPrimApi,
+  MuxPrimApi,
+  NEQPrimApi,
+  NodeApi,
+  NotPrimApi,
+  OrPrimApi,
+  XorPrimApi,
+  given
+}
+import org.llvm.mlir.scalalib.{Block, Context, LocationApi, Operation, given}
+
+import java.lang.foreign.Arena
+
+given [R <: Referable[Bool]]: BoolApi[R] with
+  extension (ref: R)
+    def unary_!(
+      using Arena,
+      Context,
+      Block,
+      sourcecode.File,
+      sourcecode.Line,
+      sourcecode.Name.Machine
+    ): Node[Bool] =
+      val op0    = summon[NotPrimApi].op(ref.refer, locate)
+      op0.operation.appendToBlock()
+      val nodeOp = summon[NodeApi].op(
+        name = valName,
+        location = locate,
+        nameKind = FirrtlNameKind.Interesting,
+        input = op0.result
+      )
+      nodeOp.operation.appendToBlock()
+      new Node[Bool]:
+        val _tpe:       Bool      = new Object with Bool
+        val _operation: Operation = nodeOp.operation
+
+    def asBits(
+      using Arena,
+      Context,
+      Block,
+      sourcecode.File,
+      sourcecode.Line,
+      sourcecode.Name.Machine
+    ): Node[Bits] =
+      val nodeOp = summon[NodeApi].op(
+        name = valName,
+        location = locate,
+        nameKind = FirrtlNameKind.Interesting,
+        input = ref.refer
+      )
+      nodeOp.operation.appendToBlock()
+      new Node[Bits]:
+        val _tpe:       Bits      = new Object with Bits:
+          private[zaozi] val _width = nodeOp.operation.getResult(0).getType.getBitWidth(true).toInt
+        val _operation: Operation = nodeOp.operation
+
+    def ===(
+      that: R
+    )(
+      using Arena,
+      Context,
+      Block,
+      sourcecode.File,
+      sourcecode.Line,
+      sourcecode.Name.Machine
+    ): Node[Bool] =
+      val op0    = summon[EQPrimApi].op(ref.refer, that.refer, locate)
+      op0.operation.appendToBlock()
+      val nodeOp = summon[NodeApi].op(
+        name = valName,
+        location = locate,
+        nameKind = FirrtlNameKind.Interesting,
+        input = op0.result
+      )
+      nodeOp.operation.appendToBlock()
+      new Node[Bool]:
+        val _tpe:       Bool      = new Object with Bool
+        val _operation: Operation = nodeOp.operation
+
+    def =/=(
+      that: R
+    )(
+      using Arena,
+      Context,
+      Block,
+      sourcecode.File,
+      sourcecode.Line,
+      sourcecode.Name.Machine
+    ): Node[Bool] =
+      val op0    = summon[NEQPrimApi].op(ref.refer, that.refer, locate)
+      op0.operation.appendToBlock()
+      val nodeOp = summon[NodeApi].op(
+        name = valName,
+        location = locate,
+        nameKind = FirrtlNameKind.Interesting,
+        input = op0.result
+      )
+      nodeOp.operation.appendToBlock()
+      new Node[Bool]:
+        val _tpe:       Bool      = new Object with Bool
+        val _operation: Operation = nodeOp.operation
+
+    def &(
+      that: R
+    )(
+      using Arena,
+      Context,
+      Block,
+      sourcecode.File,
+      sourcecode.Line,
+      sourcecode.Name.Machine
+    ): Node[Bool] =
+      val op0    = summon[AndPrimApi].op(ref.refer, that.refer, locate)
+      op0.operation.appendToBlock()
+      val nodeOp = summon[NodeApi].op(
+        name = valName,
+        location = locate,
+        nameKind = FirrtlNameKind.Interesting,
+        input = op0.result
+      )
+      nodeOp.operation.appendToBlock()
+      new Node[Bool]:
+        val _tpe:       Bool      = new Object with Bool
+        val _operation: Operation = nodeOp.operation
+
+    def |(
+      that: R
+    )(
+      using Arena,
+      Context,
+      Block,
+      sourcecode.File,
+      sourcecode.Line,
+      sourcecode.Name.Machine
+    ): Node[Bool] =
+      val op0    = summon[OrPrimApi].op(ref.refer, that.refer, locate)
+      op0.operation.appendToBlock()
+      val nodeOp = summon[NodeApi].op(
+        name = valName,
+        location = locate,
+        nameKind = FirrtlNameKind.Interesting,
+        input = op0.result
+      )
+      nodeOp.operation.appendToBlock()
+      new Node[Bool]:
+        val _tpe:       Bool      = new Object with Bool
+        val _operation: Operation = nodeOp.operation
+
+    def ^(
+      that: R
+    )(
+      using Arena,
+      Context,
+      Block,
+      sourcecode.File,
+      sourcecode.Line,
+      sourcecode.Name.Machine
+    ): Node[Bool] =
+      val op0    = summon[XorPrimApi].op(ref.refer, that.refer, locate)
+      op0.operation.appendToBlock()
+      val nodeOp = summon[NodeApi].op(
+        name = valName,
+        location = locate,
+        nameKind = FirrtlNameKind.Interesting,
+        input = op0.result
+      )
+      nodeOp.operation.appendToBlock()
+      new Node[Bool]:
+        val _tpe:       Bool      = new Object with Bool
+        val _operation: Operation = nodeOp.operation
+
+    def ?[Ret <: Data](
+      con: Referable[Ret],
+      alt: Referable[Ret]
+    )(
+      using Arena,
+      Context,
+      Block,
+      sourcecode.File,
+      sourcecode.Line,
+      sourcecode.Name.Machine
+    ): Node[Ret] =
+      val op0    = summon[MuxPrimApi].op(ref.refer, con.refer, alt.refer, locate)
+      op0.operation.appendToBlock()
+      val nodeOp = summon[NodeApi].op(
+        name = valName,
+        location = locate,
+        nameKind = FirrtlNameKind.Interesting,
+        input = op0.result
+      )
+      nodeOp.operation.appendToBlock()
+      new Node[Ret]:
+        val _tpe:       Ret       = con._tpe
+        val _operation: Operation = nodeOp.operation
+end given
---
+++ zaozi/src/default/UIntApi.scala
@@ -0,0 +1,365 @@
+// SPDX-License-Identifier: Apache-2.0
+// SPDX-FileCopyrightText: 2025 Jiuyang Liu <liu@jiuyang.me>
+package me.jiuyang.zaozi.default
+
+import me.jiuyang.zaozi.UIntApi
+import me.jiuyang.zaozi.reftpe.*
+import me.jiuyang.zaozi.valuetpe.*
+
+import org.llvm.circt.scalalib.firrtl.capi.{given_FirrtlBundleFieldApi, given_TypeApi, FirrtlNameKind}
+import org.llvm.circt.scalalib.firrtl.operation.{
+  AddPrimApi,
+  DShlPrimApi,
+  DShrPrimApi,
+  DivPrimApi,
+  EQPrimApi,
+  GEQPrimApi,
+  GTPrimApi,
+  LEQPrimApi,
+  LTPrimApi,
+  MulPrimApi,
+  NEQPrimApi,
+  NodeApi,
+  RemPrimApi,
+  ShlPrimApi,
+  ShrPrimApi,
+  SubPrimApi,
+  given
+}
+import org.llvm.mlir.scalalib.{Block, Context, LocationApi, Operation, given}
+
+import java.lang.foreign.Arena
+
+given [R <: Referable[UInt]]: UIntApi[R] with
+  extension (ref: R)
+    def asBits(
+      using Arena,
+      Context,
+      Block,
+      sourcecode.File,
+      sourcecode.Line,
+      sourcecode.Name.Machine
+    ): Node[Bits] =
+      val nodeOp = summon[NodeApi].op(
+        name = valName,
+        location = locate,
+        nameKind = FirrtlNameKind.Interesting,
+        input = ref.refer
+      )
+      nodeOp.operation.appendToBlock()
+      new Node[Bits]:
+        val _tpe:       Bits      = new Bits:
+          private[zaozi] val _width = nodeOp.operation.getResult(0).getType.getBitWidth(true).toInt
+        val _operation: Operation = nodeOp.operation
+
+    def +(
+      that: R
+    )(
+      using Arena,
+      Context,
+      Block,
+      sourcecode.File,
+      sourcecode.Line,
+      sourcecode.Name.Machine
+    ): Node[UInt] =
+      val op0    = summon[AddPrimApi].op(ref.refer, that.refer, locate)
+      op0.operation.appendToBlock()
+      val nodeOp = summon[NodeApi].op(
+        name = valName,
+        location = locate,
+        nameKind = FirrtlNameKind.Interesting,
+        input = op0.result
+      )
+      nodeOp.operation.appendToBlock()
+      new Node[UInt]:
+        val _tpe:       UInt      = new UInt:
+          private[zaozi] val _width = nodeOp.operation.getResult(0).getType.getBitWidth(true).toInt
+        val _operation: Operation = nodeOp.operation
+    def -(
+      that: R
+    )(
+      using Arena,
+      Context,
+      Block,
+      sourcecode.File,
+      sourcecode.Line,
+      sourcecode.Name.Machine
+    ): Node[UInt] =
+      val op0    = summon[SubPrimApi].op(ref.refer, that.refer, locate)
+      op0.operation.appendToBlock()
+      val nodeOp = summon[NodeApi].op(
+        name = valName,
+        location = locate,
+        nameKind = FirrtlNameKind.Interesting,
+        input = op0.result
+      )
+      nodeOp.operation.appendToBlock()
+      new Node[UInt]:
+        val _tpe:       UInt      = new UInt:
+          private[zaozi] val _width = nodeOp.operation.getResult(0).getType.getBitWidth(true).toInt
+        val _operation: Operation = nodeOp.operation
+    def *(
+      that: R
+    )(
+      using Arena,
+      Context,
+      Block,
+      sourcecode.File,
+      sourcecode.Line,
+      sourcecode.Name.Machine
+    ): Node[UInt] =
+      val op0    = summon[MulPrimApi].op(ref.refer, that.refer, locate)
+      op0.operation.appendToBlock()
+      val nodeOp = summon[NodeApi].op(
+        name = valName,
+        location = locate,
+        nameKind = FirrtlNameKind.Interesting,
+        input = op0.result
+      )
+      nodeOp.operation.appendToBlock()
+      new Node[UInt]:
+        val _tpe:       UInt      = new UInt:
+          private[zaozi] val _width = nodeOp.operation.getResult(0).getType.getBitWidth(true).toInt
+        val _operation: Operation = nodeOp.operation
+    def /(
+      that: R
+    )(
+      using Arena,
+      Context,
+      Block,
+      sourcecode.File,
+      sourcecode.Line,
+      sourcecode.Name.Machine
+    ): Node[UInt] =
+      val op0    = summon[DivPrimApi].op(ref.refer, that.refer, locate)
+      op0.operation.appendToBlock()
+      val nodeOp = summon[NodeApi].op(
+        name = valName,
+        location = locate,
+        nameKind = FirrtlNameKind.Interesting,
+        input = op0.result
+      )
+      nodeOp.operation.appendToBlock()
+      new Node[UInt]:
+        val _tpe:       UInt      = new UInt:
+          private[zaozi] val _width = nodeOp.operation.getResult(0).getType.getBitWidth(true).toInt
+        val _operation: Operation = nodeOp.operation
+    def %(
+      that: R
+    )(
+      using Arena,
+      Context,
+      Block,
+      sourcecode.File,
+      sourcecode.Line,
+      sourcecode.Name.Machine
+    ): Node[UInt] =
+      val op0    = summon[RemPrimApi].op(ref.refer, that.refer, locate)
+      op0.operation.appendToBlock()
+      val nodeOp = summon[NodeApi].op(
+        name = valName,
+        location = locate,
+        nameKind = FirrtlNameKind.Interesting,
+        input = op0.result
+      )
+      nodeOp.operation.appendToBlock()
+      new Node[UInt]:
+        val _tpe:       UInt      = new UInt:
+          private[zaozi] val _width = nodeOp.operation.getResult(0).getType.getBitWidth(true).toInt
+        val _operation: Operation = nodeOp.operation
+    def <(
+      that: R
+    )(
+      using Arena,
+      Context,
+      Block,
+      sourcecode.File,
+      sourcecode.Line,
+      sourcecode.Name.Machine
+    ): Node[Bool] =
+      val op0    = summon[LTPrimApi].op(ref.refer, that.refer, locate)
+      op0.operation.appendToBlock()
+      val nodeOp = summon[NodeApi].op(
+        name = valName,
+        location = locate,
+        nameKind = FirrtlNameKind.Interesting,
+        input = op0.result
+      )
+      nodeOp.operation.appendToBlock()
+      new Node[Bool]:
+        val _tpe:       Bool      = new Object with Bool
+        val _operation: Operation = nodeOp.operation
+    def <=(
+      that: R
+    )(
+      using Arena,
+      Context,
+      Block,
+      sourcecode.File,
+      sourcecode.Line,
+      sourcecode.Name.Machine
+    ): Node[Bool] =
+      val op0    = summon[LEQPrimApi].op(ref.refer, that.refer, locate)
+      op0.operation.appendToBlock()
+      val nodeOp = summon[NodeApi].op(
+        name = valName,
+        location = locate,
+        nameKind = FirrtlNameKind.Interesting,
+        input = op0.result
+      )
+      nodeOp.operation.appendToBlock()
+      new Node[Bool]:
+        val _tpe:       Bool      = new Object with Bool
+        val _operation: Operation = nodeOp.operation
+
+    def >(
+      that: R
+    )(
+      using Arena,
+      Context,
+      Block,
+      sourcecode.File,
+      sourcecode.Line,
+      sourcecode.Name.Machine
+    ): Node[Bool] =
+      val op0    = summon[GTPrimApi].op(ref.refer, that.refer, locate)
+      op0.operation.appendToBlock()
+      val nodeOp = summon[NodeApi].op(
+        name = valName,
+        location = locate,
+        nameKind = FirrtlNameKind.Interesting,
+        input = op0.result
+      )
+      nodeOp.operation.appendToBlock()
+      new Node[Bool]:
+        val _tpe:       Bool      = new Object with Bool
+        val _operation: Operation = nodeOp.operation
+
+    def >=(
+      that: R
+    )(
+      using Arena,
+      Context,
+      Block,
+      sourcecode.File,
+      sourcecode.Line,
+      sourcecode.Name.Machine
+    ): Node[Bool] =
+      val op0    = summon[GEQPrimApi].op(ref.refer, that.refer, locate)
+      op0.operation.appendToBlock()
+      val nodeOp = summon[NodeApi].op(
+        name = valName,
+        location = locate,
+        nameKind = FirrtlNameKind.Interesting,
+        input = op0.result
+      )
+      nodeOp.operation.appendToBlock()
+      new Node[Bool]:
+        val _tpe:       Bool      = new Object with Bool
+        val _operation: Operation = nodeOp.operation
+
+    def ===(
+      that: R
+    )(
+      using Arena,
+      Context,
+      Block,
+      sourcecode.File,
+      sourcecode.Line,
+      sourcecode.Name.Machine
+    ): Node[Bool] =
+      val op0    = summon[EQPrimApi].op(ref.refer, that.refer, locate)
+      op0.operation.appendToBlock()
+      val nodeOp = summon[NodeApi].op(
+        name = valName,
+        location = locate,
+        nameKind = FirrtlNameKind.Interesting,
+        input = op0.result
+      )
+      nodeOp.operation.appendToBlock()
+      new Node[Bool]:
+        val _tpe:       Bool      = new Object with Bool
+        val _operation: Operation = nodeOp.operation
+
+    def =/=(
+      that: R
+    )(
+      using Arena,
+      Context,
+      Block,
+      sourcecode.File,
+      sourcecode.Line,
+      sourcecode.Name.Machine
+    ): Node[Bool] =
+      val op0    = summon[NEQPrimApi].op(ref.refer, that.refer, locate)
+      op0.operation.appendToBlock()
+      val nodeOp = summon[NodeApi].op(
+        name = valName,
+        location = locate,
+        nameKind = FirrtlNameKind.Interesting,
+        input = op0.result
+      )
+      nodeOp.operation.appendToBlock()
+      new Node[Bool]:
+        val _tpe:       Bool      = new Object with Bool
+        val _operation: Operation = nodeOp.operation
+
+    def <<(
+      that: Int | Referable[UInt]
+    )(
+      using Arena,
+      Context,
+      Block,
+      sourcecode.File,
+      sourcecode.Line,
+      sourcecode.Name.Machine
+    ): Node[UInt] =
+      val nodeOp = summon[NodeApi].op(
+        name = valName,
+        location = locate,
+        nameKind = FirrtlNameKind.Interesting,
+        input = that match
+          case that: Int             =>
+            val op0 = summon[ShlPrimApi].op(ref.refer, that, locate)
+            op0.operation.appendToBlock()
+            op0.result
+          case that: Referable[UInt] =>
+            val op0 = summon[DShlPrimApi].op(ref.refer, that.refer, locate)
+            op0.operation.appendToBlock()
+            op0.result
+      )
+      nodeOp.operation.appendToBlock()
+      new Node[UInt]:
+        val _tpe:       UInt      = new UInt:
+          private[zaozi] val _width = nodeOp.operation.getResult(0).getType.getBitWidth(true).toInt
+        val _operation: Operation = nodeOp.operation
+    def >>(
+      that: Int | Referable[UInt]
+    )(
+      using Arena,
+      Context,
+      Block,
+      sourcecode.File,
+      sourcecode.Line,
+      sourcecode.Name.Machine
+    ): Node[UInt] =
+      val nodeOp = summon[NodeApi].op(
+        name = valName,
+        location = locate,
+        nameKind = FirrtlNameKind.Interesting,
+        input = that match
+          case that: Int             =>
+            val op0 = summon[ShrPrimApi].op(ref.refer, that, locate)
+            op0.operation.appendToBlock()
+            op0.result
+          case that: Referable[UInt] =>
+            val op0 = summon[DShrPrimApi].op(ref.refer, that.refer, locate)
+            op0.operation.appendToBlock()
+            op0.result
+      )
+      nodeOp.operation.appendToBlock()
+      new Node[UInt]:
+        val _tpe:       UInt      = new UInt:
+          private[zaozi] val _width = nodeOp.operation.getResult(0).getType.getBitWidth(true).toInt
+        val _operation: Operation = nodeOp.operation
+end given
