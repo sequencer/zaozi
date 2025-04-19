b95966037a3c7ee1f3059b6c54c85619e2d76898
Jiuyang Liu <liu@jiuyang.me>
2025-04-19 11:08:47 +0800

    bump circt

--- nix/overlay.nix
+++ nix/overlay.nix
@@ -19,26 +19,8 @@ in
       passthru = { inherit jre; };
     };
 
-  circt = extraNixpkgs.circt.overrideAttrs (prevAttrs: {
-    patches = (prevAttrs.patches or [ ]) ++ [
-      # [OM] Add missing CAPI omTypeIsAMapType
-      (prev.fetchpatch {
-        url = "https://github.com/llvm/circt/commit/7728eb3cd836958fe1a3f4b81da8a4d981228f71.patch";
-        sha256 = "sha256-aVGnTVuZYlz8HuXVLfeYCtgC1YnVwcVpiQKn1bJFsaA=";
-      })
-      # [OM] Fix tuple_get operation in OMEvaluator
-      (prev.fetchpatch {
-        url = "https://github.com/llvm/circt/commit/62dd6304b046527b8472d6f446b28be975624b6c.patch";
-        sha256 = "sha256-ranoZulCy+PDStJ13hWWE2I1CagTEVIsYCMnJlJq25A=";
-      })
-      # [OM] Fix finalization of nested ReferenceValue
-      (prev.fetchpatch {
-        url = "https://github.com/llvm/circt/commit/df2484d447eefe06b3c244f2786a93081c9032ef.patch";
-        sha256 = "sha256-h03X8IMUlwBG884QGoeieWq07kmqCkoHFQp+IplxbB8=";
-      })
-    ];
-  });
-
+  circt = extraNixpkgs.circt; 
+  
   circt-install = final.callPackage ./pkgs/circt-install.nix { };
 
   mlir-install = final.callPackage ./pkgs/mlir-install.nix { };
