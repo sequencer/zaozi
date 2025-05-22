4b7f27ad38c7d3dfe8bc71fb159430a5d9f7a906
Clo91eaf <Clo91eaf@qq.com>
2025-05-22 17:19:50 +0800

    [rvcover] add tests for rvcover
    Signed-off-by: Clo91eaf <Clo91eaf@qq.com>

---
+++ rvcover/tests/src/RecipeTest.scala
@@ -0,0 +1,45 @@
+// SPDX-License-Identifier: Apache-2.0
+// SPDX-FileCopyrightText: 2025 Jianhao Ye <Clo91eaf@qq.com>
+package me.jiuyang.rvcover.tests
+
+import me.jiuyang.smtlib.default.{*, given}
+import me.jiuyang.smtlib.tpe.*
+import me.jiuyang.rvcover.*
+
+import os.Path
+import java.io.{File, FileWriter}
+
+import utest.*
+
+object RecipeTest extends TestSuite:
+  val tests = Tests:
+    test("TestRecipe"):
+      rvcoverTest {
+        val r = recipe("TestRecipe") {
+          index(0) {
+            isAddi()
+            rdRange(1, 2)
+            rs1Range(1, 32)
+            imm20Range(0, 1024)
+          }
+          index(1) {
+            isAddi()
+            rdRange(1, 2)
+            rs1Range(1, 32)
+            imm20Range(0, 1024)
+          }
+          index(2) {
+            isAddi()
+            rdRange(1, 2)
+            rs1Range(1, 32)
+            imm20Range(0, 1024)
+          }
+        }
+
+        assert(r.name == "TestRecipe")
+        println(r)
+
+        val writer = new FileWriter(new File("./output.template"), true)
+        writer.write(r.toString())
+        writer.close()
+      }
