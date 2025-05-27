dbd2ad2579941338c9fa3d8f077329b8cb9819b7
Clo91eaf <Clo91eaf@qq.com>
2025-05-27 21:20:05 +0800

    [rvcover] move rvdecoderdb related tests to InstructionTest
    Signed-off-by: Clo91eaf <Clo91eaf@qq.com>

--- rvcover/tests/src/RVCoverTest.scala
+++ rvcover/tests/src/RVCoverTest.scala
@@ -20,49 +20,6 @@ import java.io.{File, FileWriter}
 
 import java.lang.foreign.Arena
 
-// set the instruction set
-def getInstructions(): Seq[Instruction] =
-  val riscvOpcodesPath: Path = Path(
-    sys.env.getOrElse(
-      "RISCV_OPCODES_INSTALL_PATH",
-      throw new RuntimeException("Environment variable RISCV_OPCODES_INSTALL_PATH not set")
-    )
-  )
-  org.chipsalliance.rvdecoderdb
-    .instructions(riscvOpcodesPath)
-    .filter(instruction =>
-      (Seq("rv_i", "rv_zicsr", "rv_zifencei", "rv_system")).contains(instruction.instructionSet.name)
-    )
-    .toSeq
-    .filter {
-      // special case for rv32 pseudo from rv64
-      case i if i.pseudoFrom.isDefined && Seq("slli", "srli", "srai").contains(i.name) => true
-      case i if i.pseudoFrom.isDefined                                                 => false
-      case _                                                                           => true
-    }
-    .sortBy(i => (i.instructionSet.name, i.name))
-
-// set argLut
-def getArgLut(): Seq[(String, org.chipsalliance.rvdecoderdb.Arg)] =
-  val riscvOpcodesPath: Path = Path(
-    sys.env.getOrElse(
-      "RISCV_OPCODES_INSTALL_PATH",
-      throw new RuntimeException("Environment variable RISCV_OPCODES_INSTALL_PATH not set")
-    )
-  )
-  org.chipsalliance.rvdecoderdb
-    .argLut(riscvOpcodesPath)
-    .toSeq
-    .sortBy(i => (i._1, i._2.name))
-
-// helper function
-def translateToCamelCase(s: String): String =
-  s.replace(".", "_")
-    .split("[^a-zA-Z0-9]")
-    .filter(_.nonEmpty)
-    .map(_.capitalize)
-    .mkString
-
 def rvcoverTest(body: (Arena, Context, Block) ?=> Unit): Unit =
   // prepare instruction map
   val instWriter = new FileWriter(new File("./output.inst"))
