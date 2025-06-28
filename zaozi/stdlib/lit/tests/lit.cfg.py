5f8721c637df0b102bfb0d216f63ab46468ddb6e
Jiuyang Liu <liu@jiuyang.me>
2025-06-28 05:45:38 -0400

    implement zaozi stdlib for BitSet and PLA Decoder

---
+++ zaozi/stdlib/lit/tests/lit.cfg.py
@@ -0,0 +1,18 @@
+import platform
+import lit.formats
+from lit.llvm import llvm_config
+from lit.llvm.subst import ToolSubst
+
+sys.path.insert(0, os.path.dirname(os.path.abspath(__file__)))
+from ScalaCliTest import ScalaCliTest
+
+config.name = 'ZAOZISTDLIB'
+config.test_format = ScalaCliTest(True)
+config.suffixes = [".scala", ".sc"]
+config.substitutions = [
+    ('%SCALAVERSION', config.scala_version),
+    ('%RUNCLASSPATH', ':'.join(config.run_classpath)),
+    ('%JAVAHOME', config.java_home),
+    ('%JAVALIBRARYPATH', ':'.join(config.java_library_path))
+]
+config.test_source_root = os.path.dirname(__file__)
