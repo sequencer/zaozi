716499136f53d63ad72ec35eebc1fa729271900c
unlsycn <unlsycn@unlsycn.com>
2026-01-06 02:02:34 +0800

    build: bump nixpkgs and enable parallel lit
    Signed-off-by: unlsycn <unlsycn@unlsycn.com>

--- nix/pkgs/espresso.nix
+++ nix/pkgs/espresso.nix
@@ -1,20 +1,24 @@
-{
-  stdenv,
-  fetchFromGitHub,
-  cmake,
-  ninja,
+{ stdenv
+, fetchFromGitHub
+, cmake
+, ninja
+, asciidoctor
 }:
-stdenv.mkDerivation rec {
-  pname = "espresso";
-  version = "2.4";
+stdenv.mkDerivation {
+  name = "espresso";
+
   nativeBuildInputs = [
     cmake
     ninja
   ];
+  buildInputs = [
+    asciidoctor
+  ];
+
   src = fetchFromGitHub {
     owner = "chipsalliance";
     repo = "espresso";
-    rev = "v${version}";
-    sha256 = "sha256-z5By57VbmIt4sgRgvECnLbZklnDDWUA6fyvWVyXUzsI=";
+    rev = "0288253ca9459539d341bb1ada10406a74efc721";
+    sha256 = "sha256-PZd5haLtqoKCcTIIiVAV0v0tHSPmpWkp6wcUgOGye98=";
   };
 }
